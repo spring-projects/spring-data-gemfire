bb26f2d6780443709501857cafbeec4b20d246f6
Costin Leau <cleau@vmware.com>
2011-02-22 19:07:38 +0200

    Gemfire implementations for Spring 3.1 caching abstraction

---
+++ src/main/java/org/springframework/data/gemfire/support/package-info.java
@@ -0,0 +1,10 @@
+
+/**
+ *
+ * Support package for Spring Gemfire integration.
+ *  
+ * <p/>Provides Spring 3.1 caching support (Cache and CacheManager implementations on top of Gemfire APIs).
+ *
+ */
+package org.springframework.data.gemfire.support;
+
