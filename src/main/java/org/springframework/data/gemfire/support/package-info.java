70d7f6845d7a42b255b7f76b67ab291d335dd803
Costin Leau <cleau@vmware.com>
2011-04-20 20:43:44 +0300

    SGF-42 + add GemfireDaoSupport class

---
+++ src/main/java/org/springframework/data/gemfire/support/package-info.java
@@ -0,0 +1,9 @@
+
+/**
+ *
+ * Classes supporting the <code>org.springframework.data.gemfire</code> package.
+ * Contains a DAO base class for GemfireTemplate usage.
+ *
+ */
+package org.springframework.data.gemfire.support;
+
