3d5f14cc3b467cc42500effae6bd5811a4f6465a
David Turanski <dturanski@gmail.com>
2011-07-07 09:53:15 -0400

    added failing ClientCacheTest

---
+++ src/test/java/org/springframework/data/gemfire/client/ClientCacheTest.java
@@ -0,0 +1,15 @@
+package org.springframework.data.gemfire.client;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("client-cache.xml")
+public class ClientCacheTest {
+    @Test
+    public void test() {
+        
+    }
+}
