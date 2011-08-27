b5789c1dd6a41f2724add94a2e60fab8f6069ba7
Costin Leau <cleau@vmware.com>
2011-08-27 19:41:12 +0300

    + update javadocs

--- src/main/java/org/springframework/data/gemfire/listener/adapter/ContinuousQueryListenerAdapter.java
+++ src/main/java/org/springframework/data/gemfire/listener/adapter/ContinuousQueryListenerAdapter.java
@@ -57,12 +57,13 @@ import com.gemstone.gemfire.cache.query.CqQuery;
  * argument.
  *
  * <pre class="code">public interface PojoListener {
- *    void handleMessage(CqEvent event);
- *    void handleMessage(Operation op);
- *    void handleMessage(Object key);
- *    void handleMessage(Object key, Object newValue);
- *    void handleMessage(Throwable th);
- *    void handleMessage(CqEvent event, Operation op, byte[] deltaValue);
+ *    void handleEvent(CqEvent event);
+ *    void handleEvent(Operation baseOp);
+ *    void handleEvent(Object key);
+ *    void handleEvent(Object key, Object newValue);
+ *    void handleEvent(Throwable th);
+ *    void handleEvent(CqEvent event, Operation baseOp, byte[] deltaValue);
+ *    void handleEvent(CqEvent event, Operation baseOp, Operation queryOp, Object key, Object newValue);
  * }</pre>
  *
  * @author Juergen Hoeller
