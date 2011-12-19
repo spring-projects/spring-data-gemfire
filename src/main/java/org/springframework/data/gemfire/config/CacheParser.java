e2690a6bc3071de6ff3cdd771c546b5e2d70f25b
Costin Leau <cleau@vmware.com>
2011-12-19 19:12:11 +0200

    + fixed incorrect parsing of pdx-serializer attribute

--- src/main/java/org/springframework/data/gemfire/config/CacheParser.java
+++ src/main/java/org/springframework/data/gemfire/config/CacheParser.java
@@ -43,7 +43,7 @@ class CacheParser extends AbstractSingleBeanDefinitionParser {
 
 		ParsingUtils.setPropertyValue(element, builder, "cache-xml-location", "cacheXml");
 		ParsingUtils.setPropertyReference(element, builder, "properties-ref", "properties");
-		ParsingUtils.setPropertyValue(element, builder, "pdx-serializer", "pdxSerializer");
+		ParsingUtils.setPropertyReference(element, builder, "pdx-serializer", "pdxSerializer");
 		ParsingUtils.setPropertyValue(element, builder, "pdx-disk-store", "pdxDiskStore");
 		ParsingUtils.setPropertyValue(element, builder, "pdx-persistent", "pdxPersistent");
 		ParsingUtils.setPropertyValue(element, builder, "pdx-read-serialized", "pdxReadSerialized");
