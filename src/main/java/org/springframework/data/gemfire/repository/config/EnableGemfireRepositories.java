d6441c70402244c300edf0a5a7a60a746c992225
Oliver Gierke <info@olivergierke.de>
2012-08-13 12:17:37 +0200

    Formatting.

--- src/main/java/org/springframework/data/gemfire/repository/config/EnableGemfireRepositories.java
+++ src/main/java/org/springframework/data/gemfire/repository/config/EnableGemfireRepositories.java
@@ -43,35 +43,28 @@ import org.springframework.data.repository.query.QueryLookupStrategy.Key;
 public @interface EnableGemfireRepositories {
 
 	/**
-	 * Alias for the {@link #basePackages()} attribute. Allows for more concise
-	 * annotation declarations e.g.:
+	 * Alias for the {@link #basePackages()} attribute. Allows for more concise annotation declarations e.g.:
 	 * {@code @EnableGemfireRepositories("org.my.pkg")} instead of
 	 * {@code @EnableGemfireRepositories(basePackages="org.my.pkg")}.
 	 */
 	String[] value() default {};
 
 	/**
-	 * Base packages to scan for annotated components. {@link #value()} is an
-	 * alias for (and mutually exclusive with) this attribute. Use
-	 * {@link #basePackageClasses()} for a type-safe alternative to String-based
-	 * package names.
+	 * Base packages to scan for annotated components. {@link #value()} is an alias for (and mutually exclusive with) this
+	 * attribute. Use {@link #basePackageClasses()} for a type-safe alternative to String-based package names.
 	 */
 	String[] basePackages() default {};
 
 	/**
-	 * Type-safe alternative to {@link #basePackages()} for specifying the
-	 * packages to scan for annotated components. The package of each class
-	 * specified will be scanned. Consider creating a special no-op marker class
-	 * or interface in each package that serves no purpose other than being
-	 * referenced by this attribute.
+	 * Type-safe alternative to {@link #basePackages()} for specifying the packages to scan for annotated components. The
+	 * package of each class specified will be scanned. Consider creating a special no-op marker class or interface in
+	 * each package that serves no purpose other than being referenced by this attribute.
 	 */
 	Class<?>[] basePackageClasses() default {};
 
 	/**
-	 * Specifies which types are eligible for component scanning. Further
-	 * narrows the set of candidate components from everything in
-	 * {@link #basePackages()} to everything in the base packages that matches
-	 * the given filter or filters.
+	 * Specifies which types are eligible for component scanning. Further narrows the set of candidate components from
+	 * everything in {@link #basePackages()} to everything in the base packages that matches the given filter or filters.
 	 */
 	Filter[] includeFilters() default {};
 
@@ -81,18 +74,16 @@ public @interface EnableGemfireRepositories {
 	Filter[] excludeFilters() default {};
 
 	/**
-	 * Returns the postfix to be used when looking up custom repository
-	 * implementations. Defaults to {@literal Impl}. So for a repository named
-	 * {@code PersonRepository} the corresponding implementation class will be
-	 * looked up scanning for {@code PersonRepositoryImpl}.
+	 * Returns the postfix to be used when looking up custom repository implementations. Defaults to {@literal Impl}. So
+	 * for a repository named {@code PersonRepository} the corresponding implementation class will be looked up scanning
+	 * for {@code PersonRepositoryImpl}.
 	 * 
 	 * @return
 	 */
 	String repositoryImplementationPostfix() default "Impl";
 
 	/**
-	 * Configures the location of where to find the Spring Data named queries
-	 * properties file. Will default to
+	 * Configures the location of where to find the Spring Data named queries properties file. Will default to
 	 * {@code META-INFO/jpa-named-queries.properties}.
 	 * 
 	 * @return
@@ -100,16 +91,16 @@ public @interface EnableGemfireRepositories {
 	String namedQueriesLocation() default "";
 
 	/**
-	 * Returns the key of the {@link QueryLookupStrategy} to be used for lookup
-	 * queries for query methods. Defaults to {@link Key#CREATE_IF_NOT_FOUND}.
+	 * Returns the key of the {@link QueryLookupStrategy} to be used for lookup queries for query methods. Defaults to
+	 * {@link Key#CREATE_IF_NOT_FOUND}.
 	 * 
 	 * @return
 	 */
 	Key queryLookupStrategy() default Key.CREATE_IF_NOT_FOUND;
 
 	/**
-	 * Returns the {@link FactoryBean} class to be used for each repository
-	 * instance. Defaults to {@link GemfireRepositoryFactoryBean}.
+	 * Returns the {@link FactoryBean} class to be used for each repository instance. Defaults to
+	 * {@link GemfireRepositoryFactoryBean}.
 	 * 
 	 * @return
 	 */
@@ -117,9 +108,8 @@ public @interface EnableGemfireRepositories {
 
 	// Gemfire specific configuration
 	/**
-	 * Configures the name of the {@link GemfireMappingContext} bean definition
-	 * to be used to create repositories discovered through this annotation. If
-	 * not configured a default one will be created.
+	 * Configures the name of the {@link GemfireMappingContext} bean definition to be used to create repositories
+	 * discovered through this annotation. If not configured a default one will be created.
 	 * 
 	 * @return
 	 */
