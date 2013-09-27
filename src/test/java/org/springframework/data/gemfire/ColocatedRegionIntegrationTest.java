408c385aa902e19468036b18c9e85feca5902c2a
jxblum <jblum@gopivotal.com>
2013-09-27 15:16:29 -0700

    Added copyright information and corrected version, setting to 1.3.3.

--- src/test/java/org/springframework/data/gemfire/ColocatedRegionIntegrationTest.java
+++ src/test/java/org/springframework/data/gemfire/ColocatedRegionIntegrationTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2010-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.springframework.data.gemfire;
 
 import static org.junit.Assert.*;
@@ -21,7 +37,7 @@ import com.gemstone.gemfire.cache.Region;
  * @see org.junit.runner.RunWith
  * @see org.springframework.test.context.ContextConfiguration
  * @see org.springframework.test.context.junit4.SpringJUnit4ClassRunner
- * @since 1.3.2
+ * @since 1.3.3
  */
 @ContextConfiguration("/org/springframework/data/gemfire/colocated-region.xml")
 @RunWith(SpringJUnit4ClassRunner.class)
