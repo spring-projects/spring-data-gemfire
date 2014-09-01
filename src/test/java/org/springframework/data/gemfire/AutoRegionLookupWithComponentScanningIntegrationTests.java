6a72a2519461edb963d8817af74c3e0d50801b8e
John Blum <jblum@pivotal.io>
2014-09-01 15:50:55 -0700

    Additional test case to test SDG's auto Region lookup behavior with Spring's component-scan functionality. (cherry picked from commit eb9ebec574dd81a5e2e39907667b0de7f62d2171)
    Signed-off-by: John Blum <jblum@pivotal.io>

---
+++ src/test/java/org/springframework/data/gemfire/AutoRegionLookupWithComponentScanningIntegrationTests.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2010-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.data.gemfire;
+
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * The AutoRegionLookupWithComponentScanningIntegrationTests class is a test suite class testing the behavior of
+ * Spring Data GemFire's auto Region lookup behavior with Spring component scanning functionality.
+ *
+ * @author John Blum
+ * @see org.junit.Test
+ * @see org.junit.runner.RunWith
+ * @see org.springframework.test.context.ContextConfiguration
+ * @see org.springframework.test.context.junit4.SpringJUnit4ClassRunner
+ * @since 1.5.0
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
+@SuppressWarnings("unused")
+public class AutoRegionLookupWithComponentScanningIntegrationTests {
+
+	@Autowired
+	private ApplicationContext context;
+
+	@Test
+	public void testAutowiredNativeRegions() {
+		assertTrue("The 'autoRegionLookupDao' Spring bean DAO was not properly configured an initialized!",
+			context.containsBean("autoRegionLookupDao"));
+		assertNotNull(context.getBean("autoRegionLookupDao", AutoRegionLookupDao.class));
+	}
+
+}
