c0aed1984c7d928a5edd6f4bdde2067c74376ce4
John Blum <jblum@pivotal.io>
2015-02-06 15:30:26 -0800

    SGF-353 - Prepare SDG 1.7 and upgrade to GemFire 8.1.0 GA

--- src/main/java/org/springframework/data/gemfire/wan/GatewaySenderWrapper.java
+++ src/main/java/org/springframework/data/gemfire/wan/GatewaySenderWrapper.java
@@ -1,3 +1,16 @@
+/*
+ * Copyright 2002-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
 package org.springframework.data.gemfire.wan;
 
 import java.util.List;
@@ -47,7 +60,12 @@ public class GatewaySenderWrapper implements GatewaySender {
         delegate.resume();
     }
 
-    @Override
+	@Override
+	public void rebalance() {
+		delegate.rebalance();
+	}
+
+	@Override
     public boolean isRunning() {
         return delegate.isRunning();
     }
@@ -157,6 +175,7 @@ public class GatewaySenderWrapper implements GatewaySender {
     }
 
     @Override
+	@SuppressWarnings("deprecation")
     public Gateway.OrderPolicy getOrderPolicy() {
         return delegate.getOrderPolicy();
     }
