be6df696e521ebc773f127c72a9cc351c8337976
John Blum <jblum@pivotal.io>
2015-07-15 18:21:06 -0700

    SGF-409 - Modify the GemfireDataSourcePostProcessor (basis for <gfe:datasource>) to not assume a GemFire Server was configured and bootstrapped with Spring and subsequently that the SDG ListRegionsOnServerFunction was registered.

---
+++ src/main/java/org/springframework/data/gemfire/util/CollectionUtils.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2010-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.data.gemfire.util;
+
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * The CollectionUtils class is a utility class for working with Java Collections Framework and classes.
+ *
+ * @author John Blum
+ * @see java.util.Collection
+ * @see java.util.Collections
+ * @see org.springframework.util.CollectionUtils
+ * @since 1.7.0
+ */
+@SuppressWarnings("unused")
+public abstract class CollectionUtils extends org.springframework.util.CollectionUtils {
+
+	/**
+	 * A null-safe operation returning the original Iterable object if non-null or a default, empty Iterable
+	 * implementation if null.
+	 *
+	 * @param <T> the class type of the iterable elements.
+	 * @param iterable the Iterable object evaluated for a null reference.
+	 * @return the Iterable object if not null or a default, empty Iterable implementation otherwise.
+	 * @see java.lang.Iterable
+	 * @see java.util.Iterator
+	 */
+	public static <T> Iterable<T> nullSafeIterable(Iterable<T> iterable) {
+		return (iterable != null ? iterable : new Iterable<T>() {
+			@Override public Iterator<T> iterator() {
+				return new Iterator<T>() {
+					@Override public boolean hasNext() {
+						return false;
+					}
+
+					@Override public T next() {
+						throw new NoSuchElementException("no elements in this Iterator");
+					}
+
+					@Override  public void remove() {
+						throw new UnsupportedOperationException("operation not supported");
+					}
+				};
+			}
+		});
+	}
+
+	/**
+	 * A null-safe operation returning the original Collection if non-null or an empty Collection
+	 * (implemented with List) if null.
+	 *
+	 * @param <T> the element class type of the Collection.
+	 * @param collection the Collection to evaluate for being null.
+	 * @return the given Collection if not null, otherwise return an empty Collection (List).
+	 * @see java.util.Collections#emptyList()
+	 */
+	public static <T> Collection<T> nullSafeCollection(final Collection<T> collection) {
+		return (collection != null ? collection : Collections.<T>emptyList());
+	}
+
+}
---
+++ src/test/java/org/springframework/data/gemfire/util/CollectionUtilsTest.java
@@ -0,0 +1,104 @@
+/*
+ * Copyright 2010-2013 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.springframework.data.gemfire.util;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.CoreMatchers.sameInstance;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import org.junit.Test;
+
+/**
+ * The CollectionUtilsTest class is a test suite of test cases testing the contract and functionality
+ * of the CollectionUtils class.
+ *
+ * @author John Blum
+ * @see java.util.Collection
+ * @see java.util.Iterator
+ * @see org.junit.Test
+ * @see org.mockito.Mockito
+ * @see org.springframework.data.gemfire.util.CollectionUtils
+ * @since 1.7.0
+ */
+public class CollectionUtilsTest {
+
+	@Test
+	@SuppressWarnings("unchecked")
+	public void nullSafeIterableWithNonNullIterable() {
+		Iterable<Object> mockIterable = mock(Iterable.class);
+		assertThat(CollectionUtils.nullSafeIterable(mockIterable), is(sameInstance(mockIterable)));
+	}
+
+	@Test
+	public void nullSafeIterableWithNullIterable() {
+		Iterable<Object> iterable = CollectionUtils.nullSafeIterable(null);
+		assertThat(iterable, is(not(nullValue())));
+		assertThat(iterable.iterator(), is(not(nullValue())));
+	}
+
+	@Test(expected = UnsupportedOperationException.class)
+	public void nullSafeIterableIterator() {
+		Iterator<Object> iterator = CollectionUtils.nullSafeIterable(null).iterator();
+
+		assertThat(iterator, is(not(nullValue())));
+		assertThat(iterator.hasNext(), is(equalTo(false)));
+
+		try {
+			iterator.next();
+		}
+		catch (NoSuchElementException ignore) {
+			assertThat(ignore.getMessage(), is(equalTo("no elements in this Iterator")));
+			assertThat(ignore.getCause(), is(nullValue()));
+
+			try {
+				iterator.remove();
+			}
+			catch (UnsupportedOperationException expected) {
+				assertThat(expected.getMessage(), is(equalTo("operation not supported")));
+				assertThat(expected.getCause(), is(nullValue()));
+				throw expected;
+			}
+		}
+	}
+
+	@Test
+	public void nullSafeCollectionWithNonNullCollection() {
+		List<?> mockList = mock(List.class);
+		assertSame(mockList, CollectionUtils.nullSafeCollection(mockList));
+	}
+
+	@Test
+	public void nullSafeCollectionWithNullCollection() {
+		Collection collection = CollectionUtils.nullSafeCollection(null);
+
+		assertNotNull(collection);
+		assertTrue(collection.isEmpty());
+	}
+
+}
