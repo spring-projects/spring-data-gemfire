0bf13f7e0bd77d1f2a6f61e1f85aa1523cc33f39
John Blum <jblum@pivotal.io>
2015-08-10 17:44:27 -0700

    SGF-425 -  Allow early initialization and re-initialization of LazyWiringDeclarableSupport instances. (cherry picked from commit 93280bf2047081e5387a896e27e19e5d7e5cce12)
    Signed-off-by: John Blum <jblum@pivotal.io>

--- src/main/java/org/springframework/data/gemfire/LazyWiringDeclarableSupport.java
+++ src/main/java/org/springframework/data/gemfire/LazyWiringDeclarableSupport.java
@@ -19,17 +19,15 @@ package org.springframework.data.gemfire;
 import java.util.Properties;
 import java.util.concurrent.atomic.AtomicReference;
 
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
 import org.springframework.beans.factory.wiring.BeanConfigurerSupport;
 import org.springframework.beans.factory.wiring.BeanWiringInfo;
 import org.springframework.beans.factory.wiring.BeanWiringInfoResolver;
 import org.springframework.context.ApplicationListener;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.data.gemfire.support.SpringContextBootstrappingInitializer;
 import org.springframework.util.Assert;
-import org.springframework.util.ObjectUtils;
 import org.springframework.util.StringUtils;
 
 import com.gemstone.gemfire.cache.Declarable;
@@ -41,10 +39,13 @@ import com.gemstone.gemfire.cache.Declarable;
  * @author John Blum
  * @see java.util.Properties
  * @see org.springframework.beans.factory.DisposableBean
+ * @see org.springframework.beans.factory.wiring.BeanConfigurerSupport
+ * @see org.springframework.beans.factory.wiring.BeanWiringInfo
  * @see org.springframework.context.ApplicationListener
  * @see org.springframework.context.event.ContextRefreshedEvent
  * @see org.springframework.data.gemfire.DeclarableSupport
  * @see org.springframework.data.gemfire.WiringDeclarableSupport
+ * @see org.springframework.data.gemfire.support.SpringContextBootstrappingInitializer
  * @see com.gemstone.gemfire.cache.Declarable
  * @since 1.3.4
  */
@@ -52,14 +53,17 @@ import com.gemstone.gemfire.cache.Declarable;
 public abstract class LazyWiringDeclarableSupport implements ApplicationListener<ContextRefreshedEvent>,
 		Declarable, DisposableBean {
 
-	// The name of the template bean defined in the Spring context for wiring this Declarable instance.
+	// name of the template bean defined in the Spring context for wiring this Declarable instance.
 	protected static final String BEAN_NAME_PARAMETER = "bean-name";
 
-	// atomic reference to the parameter passed by GemFire when this Declared was constructed
+	// atomic reference to the parameter passed by GemFire when this Declarable was constructed
 	// and the init method was called.
 	private final AtomicReference<Properties> parametersReference = new AtomicReference<Properties>();
 
+	// condition determining the initialization state of this Declarable
 	volatile boolean initialized = false;
+
+	private String factoryKey = null;
 
 	/**
 	 * Constructs an instance of the LazyWiringDeclarableSupport class registered with the
@@ -76,6 +80,30 @@ public abstract class LazyWiringDeclarableSupport implements ApplicationListener
 	}
 
 	/**
+	 * Set the key used to locate (lookup) the Spring BeanFactory.
+	 *
+	 * @param factoryKey the key used to locate the Spring BeanFactory.
+	 * @see org.springframework.beans.factory.BeanFactory
+	 * @see org.springframework.data.gemfire.GemfireBeanFactoryLocator
+	 * @see #getFactoryKey()
+	 */
+	public final void setFactoryKey(final String factoryKey) {
+		this.factoryKey = factoryKey;
+	}
+
+	/**
+	 * Gets the key used to locate (lookup) the Spring BeanFactory.
+	 *
+	 * @return the key used to locate the Spring BeanFactory.
+	 * @see org.springframework.beans.factory.BeanFactory
+	 * @see org.springframework.data.gemfire.GemfireBeanFactoryLocator
+	 * @see #setFactoryKey(String)
+	 */
+	protected String getFactoryKey() {
+		return factoryKey;
+	}
+
+	/**
 	 * Asserts that this Declarable object has been properly configured and initialized by the Spring container
 	 * after GemFire has constructed this Declarable object during startup.  It is recommended to call this method
 	 * in any GemFire CacheCallback/Declarable object operational method (e.g. CacheLoader.load(..)) before use
@@ -88,7 +116,7 @@ public abstract class LazyWiringDeclarableSupport implements ApplicationListener
 	 */
 	protected void assertInitialized() {
 		Assert.state(isInitialized(), String.format(
-			"This Declarable object (%1$s) has not been properly configured and initialized!",
+			"This Declarable object (%1$s) has not been properly configured and initialized",
 				getClass().getName()));
 	}
 
@@ -104,7 +132,7 @@ public abstract class LazyWiringDeclarableSupport implements ApplicationListener
 	 */
 	protected void assertUninitialized() {
 		Assert.state(!isInitialized(), String.format(
-			"This Declarable object (%1$s) has already been configured and initialized, and is currently active!",
+			"This Declarable object (%1$s) has already been configured and initialized",
 				getClass().getName()));
 	}
 
@@ -125,32 +153,36 @@ public abstract class LazyWiringDeclarableSupport implements ApplicationListener
 	 * @see org.springframework.beans.factory.wiring.BeanWiringInfo
 	 * @see org.springframework.beans.factory.wiring.BeanWiringInfoResolver
 	 */
-	/* package-private */ void doInit(final ConfigurableListableBeanFactory beanFactory, final Properties parameters) {
-		BeanConfigurerSupport beanConfigurer = new BeanConfigurerSupport();
+	void doInit(final BeanFactory beanFactory, final Properties parameters) {
+		synchronized (this) {
+			if (isNotInitialized()) {
+				BeanConfigurerSupport beanConfigurer = new BeanConfigurerSupport();
 
-		beanConfigurer.setBeanFactory(beanFactory);
+				beanConfigurer.setBeanFactory(beanFactory);
 
-		final String templateBeanName = parameters.getProperty(BEAN_NAME_PARAMETER);
+				final String templateBeanName = parameters.getProperty(BEAN_NAME_PARAMETER);
 
-		if (StringUtils.hasText(templateBeanName)) {
-			if (beanFactory.containsBean(templateBeanName)) {
-				beanConfigurer.setBeanWiringInfoResolver(new BeanWiringInfoResolver() {
-					@Override public BeanWiringInfo resolveWiringInfo(final Object beanInstance) {
-						return new BeanWiringInfo(templateBeanName);
+				if (StringUtils.hasText(templateBeanName)) {
+					if (beanFactory.containsBean(templateBeanName)) {
+						beanConfigurer.setBeanWiringInfoResolver(new BeanWiringInfoResolver() {
+							@Override public BeanWiringInfo resolveWiringInfo(final Object beanInstance) {
+								return new BeanWiringInfo(templateBeanName);
+							}
+						});
 					}
-				});
-			}
-			else {
-				throw new IllegalArgumentException(String.format(
-					"No bean with name '%1$s' was found in the Spring context '%2$s'.", templateBeanName, beanFactory));
+					else {
+						throw new IllegalArgumentException(String.format(
+							"No bean with name '%1$s' was found in the Spring context '%2$s'.", templateBeanName, beanFactory));
+					}
+				}
+
+				beanConfigurer.afterPropertiesSet();
+				beanConfigurer.configureBean(this);
+				beanConfigurer.destroy();
+
+				initialized = true;
 			}
 		}
-
-		beanConfigurer.afterPropertiesSet();
-		beanConfigurer.configureBean(this);
-		beanConfigurer.destroy();
-
-		initialized = true;
 
 		doPostInit(parameters);
 	}
@@ -161,7 +193,7 @@ public abstract class LazyWiringDeclarableSupport implements ApplicationListener
 	 *
 	 * @param parameters Properties instance containing the parameters from GemFire's configuration file
 	 * (e.g. cache.xml) to configure and initialize this Declarable object.
-	 * @see #doInit(org.springframework.beans.factory.config.ConfigurableListableBeanFactory, java.util.Properties)
+	 * @see #doInit(BeanFactory, Properties)
 	 */
 	protected void doPostInit(final Properties parameters) {
 	}
@@ -174,26 +206,58 @@ public abstract class LazyWiringDeclarableSupport implements ApplicationListener
 	 * Declarable as a Properties instance.
 	 * @throws IllegalStateException if this Declarable object has already been configured/initialized
 	 * by the Spring container and is currently active.
-	 * @see #doInit(org.springframework.beans.factory.config.ConfigurableListableBeanFactory, java.util.Properties)
+	 * @see #doInit(BeanFactory, Properties)
 	 * @see java.util.Properties
 	 */
 	@Override
 	public final void init(final Properties parameters) {
-		assertUninitialized();
 		parametersReference.set(parameters);
+
+		try {
+			doInit(locateBeanFactory(getFactoryKey()), nullSafeGetParameters());
+		}
+		catch (IllegalStateException ignore) {
+			// the BeanFactory does not exist or has been released and or closed, so ignore
+		}
 	}
 
 	/**
 	 * Determines whether this Declarable object has been configured and initialized (i.e. the doInit method
 	 * has been called) by the Spring container.
 	 *
-	 * @return a boolean value indicating whether this Declarable object has been configured and initialized by
-	 * the Spring container.
+	 * @return a boolean value indicating whether this Declarable object has been configured and initialized
+	 * by the Spring container.
 	 * @see #assertInitialized()
-	 * @see #doInit(org.springframework.beans.factory.config.ConfigurableListableBeanFactory, java.util.Properties)
+	 * @see #doInit(BeanFactory, Properties)
 	 */
 	protected boolean isInitialized() {
 		return initialized;
+	}
+
+	/**
+	 * Determines whether this Declarable object has been configured and initialized (i.e. the doInit method
+	 * has been called) by the Spring container.
+	 *
+	 * @return a boolean value indicating whether this Declarable object has been configured and initialized
+	 * by the Spring container.
+	 * @see #doInit(BeanFactory, Properties)
+	 * @see #isInitialized()
+	 */
+	protected boolean isNotInitialized() {
+		return !isInitialized();
+	}
+
+	/**
+	 * Locates an existing Spring BeanFactory.
+	 *
+	 * @param factoryKey the key used to locate (lookup) the Spring BeanFactory.
+	 * @return a reference to the Spring BeanFactory if it exists.
+	 * @throws IllegalStateException if the BeanFactory has already been released or closed.
+	 * @see org.springframework.data.gemfire.GemfireBeanFactoryLocator
+	 * @see org.springframework.beans.factory.BeanFactory
+	 */
+	protected BeanFactory locateBeanFactory(String factoryKey) {
+		return new GemfireBeanFactoryLocator().useBeanFactory(factoryKey).getFactory();
 	}
 
 	/**
@@ -216,19 +280,15 @@ public abstract class LazyWiringDeclarableSupport implements ApplicationListener
 	 * @param event the ContextRefreshedEvent published by the Spring ApplicationContext after it is successfully
 	 * created and initialized by GemFire.
 	 * @throws IllegalArgumentException if the ApplicationContext is not an instance of ConfigurableApplicationContext.
-	 * @see #doInit(org.springframework.beans.factory.config.ConfigurableListableBeanFactory, java.util.Properties)
+	 * @see #doInit(BeanFactory, Properties)
 	 * @see #nullSafeGetParameters()
 	 * @see org.springframework.context.ApplicationListener#onApplicationEvent(org.springframework.context.ApplicationEvent)
 	 * @see org.springframework.context.event.ContextRefreshedEvent
 	 */
 	@Override
 	public final void onApplicationEvent(final ContextRefreshedEvent event) {
-		Assert.isTrue(event.getApplicationContext() instanceof ConfigurableApplicationContext,
-			String.format("The Spring ApplicationContext (%1$s) must be an instance of ConfigurableApplicationContext.",
-				ObjectUtils.nullSafeClassName(event.getApplicationContext())));
-
-		doInit(((ConfigurableApplicationContext) event.getApplicationContext()).getBeanFactory(),
-			nullSafeGetParameters());
+		Assert.notNull(event.getApplicationContext(), "The Spring ApplicationContext must not be null");
+		doInit(event.getApplicationContext(), nullSafeGetParameters());
 	}
 
 	/**
