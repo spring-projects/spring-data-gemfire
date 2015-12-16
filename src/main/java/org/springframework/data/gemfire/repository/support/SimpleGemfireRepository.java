eaed096f09d2f4c399589745d107d32c3b493eef
Oliver Gierke <ogierke@gopivotal.com>
2015-12-16 14:59:44 +0100

    SGF-455 - Simplified registration for shared GemfireMappingContext for repositories.
    A <gemfire:repositories /> now registers a single bean definition for a GemfireMappingContext that gets wired to the GemfireRepositoryFactoryBean instances registered within the system unless an explicit mapping context is referenced.
    
    The GemfireRepositoryFactoryBean now checks a GemfireMappingContext being registered and rejects to start without one. The same applies to GemfireRepositoryFactory.
    
    Related tickets: SGF-448, SGF-449, SGF-450.

--- src/main/java/org/springframework/data/gemfire/repository/support/SimpleGemfireRepository.java
+++ src/main/java/org/springframework/data/gemfire/repository/support/SimpleGemfireRepository.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2012-2015 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.springframework.data.gemfire.repository.support;
 
 import java.io.Serializable;
@@ -36,7 +51,6 @@ import com.gemstone.gemfire.cache.query.SelectResults;
 public class SimpleGemfireRepository<T, ID extends Serializable> implements GemfireRepository<T, ID> {
 
 	private final GemfireTemplate template;
-
 	private final EntityInformation<T, ID> entityInformation;
 
 	/**
@@ -46,6 +60,7 @@ public class SimpleGemfireRepository<T, ID extends Serializable> implements Gemf
 	 * @param entityInformation must not be {@literal null}.
 	 */
 	public SimpleGemfireRepository(GemfireTemplate template, EntityInformation<T, ID> entityInformation) {
+		
 		Assert.notNull(template);
 		Assert.notNull(entityInformation);
 
