53234a6d048e6f7420f8d13b12c170488e2e2dff
John Blum <jblum@pivotal.io>
2016-09-07 11:17:34 -0700

    SGF-514 - Configure HttpService with annotations.
    (cherry picked from commit f39912a44ca8eb588bddc11cb7606b8d409f220c)
    Signed-off-by: John Blum <jblum@pivotal.io>

---
+++ src/main/java/org/springframework/data/gemfire/config/annotation/HttpServiceConfiguration.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2012 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.springframework.data.gemfire.config.annotation;
+
+import java.util.Map;
+import java.util.Properties;
+
+import org.springframework.data.gemfire.config.annotation.support.EmbeddedServiceConfigurationSupport;
+import org.springframework.data.gemfire.util.PropertiesBuilder;
+
+/**
+ * The HttpServiceConfiguration class is a Spring {@link org.springframework.context.annotation.ImportBeanDefinitionRegistrar}
+ * that applies additional GemFire/Geode configuration by way of GemFire/Geode System properties to configure
+ * GemFire/Geode's embeded HTTP service and services.
+ *
+ * @author John Blum
+ * @see org.springframework.data.gemfire.config.annotation.EnableHttpService
+ * @see org.springframework.data.gemfire.config.annotation.support.EmbeddedServiceConfigurationSupport
+ * @see <a href="http://geode.docs.pivotal.io/docs/rest_apps/book_intro.html">Developing REST Applications for Apache Geode</a>
+ * @since 1.9.0
+ */
+public class HttpServiceConfiguration extends EmbeddedServiceConfigurationSupport {
+
+	public static final boolean DEFAULT_HTTP_SERVICE_SSL_REQUIRE_AUTHENTICATION = false;
+	public static final boolean DEFAULT_HTTP_SERVICE_START_DEVELOPER_REST_API = false;
+
+	public static final int DEFAULT_HTTP_SERVICE_PORT = 7070;
+
+	/* (non-Javadoc) */
+	@Override
+	protected Class getAnnotationType() {
+		return EnableHttpService.class;
+	}
+
+	/* (non-Javadoc) */
+	@Override
+	protected Properties toGemFireProperties(Map<String, Object> annotationAttributes) {
+		PropertiesBuilder gemfireProperties = PropertiesBuilder.create();
+
+		gemfireProperties.setProperty("http-service-bind-address", annotationAttributes.get("bindAddress"));
+
+		gemfireProperties.setPropertyIfNotDefault("http-service-port",
+			annotationAttributes.get("port"), DEFAULT_HTTP_SERVICE_PORT);
+
+		gemfireProperties.setPropertyIfNotDefault("http-service-ssl-require-authentication",
+			annotationAttributes.get("sslRequireAuthentication"), DEFAULT_HTTP_SERVICE_SSL_REQUIRE_AUTHENTICATION);
+
+		gemfireProperties.setPropertyIfNotDefault("start-dev-rest-api",
+			annotationAttributes.get("startDeveloperRestApi"), DEFAULT_HTTP_SERVICE_START_DEVELOPER_REST_API);
+
+		return gemfireProperties.build();
+	}
+}
