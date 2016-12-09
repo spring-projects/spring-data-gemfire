ccca168c6e0a82c259b6cae72a14ab367760d462
John Blum <jblum@pivotal.io>
2016-12-09 14:01:54 -0800

    SGF-106 - Add annotation support for creating Indexes.
    (cherry picked from commit ef5dd8b7cef32daa12c48e4b0d339bd90059b14f)
    Signed-off-by: John Blum <jblum@pivotal.io>

--- src/test/java/org/springframework/data/gemfire/config/annotation/test/entities/PartitionRegionEntity.java
+++ src/test/java/org/springframework/data/gemfire/config/annotation/test/entities/PartitionRegionEntity.java
@@ -18,6 +18,8 @@
 package org.springframework.data.gemfire.config.annotation.test.entities;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.gemfire.IndexType;
+import org.springframework.data.gemfire.mapping.Indexed;
 import org.springframework.data.gemfire.mapping.PartitionRegion;
 
 /**
@@ -38,7 +40,10 @@ public class PartitionRegionEntity {
 	@Id
 	private Long id;
 
+	@Indexed(expression = "first_name", from = "/LoyalCustomers", type = IndexType.FUNCTIONAL)
 	private String firstName;
+
+	@Indexed(name = "LastNameIdx")
 	private String lastName;
 
 }
