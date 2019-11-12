9a4ba1d68c5568e918c1c90a4c52e56714651245
John Blum <jblum@pivotal.io>
2019-11-11 20:19:38 -0800

    SGF-894 - Move TransactionApplicationEvent to o.s.d.g.transaction.event.

--- src/main/java/org/springframework/data/gemfire/transaction/event/TransactionApplicationEvent.java
+++ src/main/java/org/springframework/data/gemfire/transaction/event/TransactionApplicationEvent.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.springframework.data.gemfire.transaction;
+package org.springframework.data.gemfire.transaction.event;
 
 import java.time.Instant;
 import java.time.LocalDateTime;
