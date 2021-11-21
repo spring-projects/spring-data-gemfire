264859329a1dfa1376f4a19541e7dd73e3c4ec37
Charles <charleszkq@gmail.com>
2021-11-20 21:58:14 -0600

    fix flaky test

--- src/test/java/org/springframework/data/gemfire/config/support/DefinedIndexesIntegrationTests.java
+++ src/test/java/org/springframework/data/gemfire/config/support/DefinedIndexesIntegrationTests.java
@@ -114,7 +114,8 @@ public class DefinedIndexesIntegrationTests {
 		QueryService queryService = gemfireCache.getQueryService();
 
 		List<String> expectedDefinedIndexNames = Arrays.asList(id.getName(), birthDate.getName(), name.getName());
-
+		expectedDefinedIndexNames.sort((a, b) -> (a.compareTo(b)));
+		definedIndexNames.sort((a, b) -> (a.compareTo(b)));
 		assertThat(definedIndexNames).isEqualTo(expectedDefinedIndexNames);
 		assertThat(id).isEqualTo(queryService.getIndex(people, id.getName()));
 		assertThat(birthDate).isEqualTo(queryService.getIndex(people, birthDate.getName()));
